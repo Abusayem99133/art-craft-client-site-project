aa87914408d1b46a0dfa91cb2ccd2b69f962cb12
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-04-25 22:23:05 +0600

    crart items section work on going

--- src/Components/Routings/Homes/Home.jsx
+++ src/Components/Routings/Homes/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import CraftItems from "./CraftItems";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <h1 className="text3-xl">hlw world a</h1>
+      <div>{<CraftItems></CraftItems>}</div>
     </div>
   );
 };
